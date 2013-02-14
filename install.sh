cd96e7322dad6649fe5a233fcd55429a53065c09
Oliver Friedrich <oliver.friedrich@igd-r.fraunhofer.de>
2013-02-14 14:16:35 +0100

    Test with Valloric/YouCompleteMe

--- install.sh
+++ install.sh
@@ -38,3 +38,9 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qa
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+### VIM Plugin: https://github.com/Valloric/YouCompleteMe
+## Mac: brew install cmake
+## Ubuntu/Debian: apt-get install python-dev cmake
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer
