0e9625ef276a14b2ec4475d2dd8f8f5ba36a3943
Oliver Friedrich <oliver.friedrich@igd-r.fraunhofer.de>
2014-11-24 15:09:40 +0100

    remove shell fuzzy finder

--- install.sh
+++ install.sh
@@ -48,6 +48,6 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 #./install.sh --clang-completer
 
 # add Fuzzy finder for the shell
-cd ~/
-git clone https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#cd ~/
+#git clone https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
